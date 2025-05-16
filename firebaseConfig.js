0d49a901e70ab6761e5e6de6ec0430cb9e6962a9
bogdanovic91 <bogdanovicn@bettercollective.com>
2025-05-16 16:29:38 +0200

    Add Firebase and Expo dependencies: update package.json and package-lock.json to include expo-application, expo-auth-session, expo-crypto, and firebase. Modify Android build.gradle to apply Google services plugin and update iOS project.pbxproj to include GoogleService-Info.plist and adjust build phases for new resources.

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlpxkhXpJqxKjdJ4YUnZ4lf9LaMhidpS8",
+  authDomain: "knguru-4cd17.firebaseapp.com",
+  projectId: "knguru-4cd17",
+  storageBucket: "knguru-4cd17.firebasestorage.app",
+  messagingSenderId: "830899972117",
+  appId: "1:830899972117:web:63e393e9cab363d185061c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+//IOS 620102760932-905tivopfp46bgjobn3prl5kifgv55qf.apps.googleusercontent.com
